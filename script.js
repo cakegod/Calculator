7ae64bdf8c196221c67be76b308daba4a38880da
Cake <cakemancerr@gmail.com>
2021-06-05 21:19:34 +0200

    Improved design, fixed dot to one per number

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+"use strict";
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 const numbers = document.querySelectorAll(".numbers");
@@ -6,6 +6,8 @@ const operations = document.querySelectorAll(".operations");
 const clear = document.querySelector(".clear");
 const enter = document.querySelector(".enter");
 const dot = document.querySelector(".dot");
+let num1dot = false;
+let num2dot = false;
 let result;
 let num1 = "";
 let operator = "";
@@ -23,17 +25,22 @@ numbers.forEach(numbers =>
             num2 += numbers.textContent;
         num2 = parseFloat(num2);
         return display.append(numbers.textContent);
+
     }
     ));
 
 // to listen to operations button
 operations.forEach(operations =>
     operations.addEventListener("click", () => {
-        if (operator == "") {
+        if (operator == "" && num1 != "") {
             operator = operations.textContent;
-            return display.append(operations.textContent);
-        } else if (operator != "")
+            return display.append(" " + operations.textContent + " ");
+        } else if (num1 != "" && num2 != "") {
             operation(num1, num2, operator);
+            operator = operations.textContent;
+            display.append(" " + operations.textContent + " ");
+        } 
+
 
 
     }));
@@ -45,16 +52,24 @@ clear.addEventListener("click", () => {
     operator = "";
     num2 = "";
     result = "";
+    num1dot = false;
+    num2dot = false;
 });
 
 // to give the value
 enter.addEventListener("click", () => operation(num1, num2, operator));
 
 dot.addEventListener("click", () => {
-    display.append(dot.textContent);
-    if (operator == "") {
+
+    if (operator == "" && num1 != "" && num1dot == false) {
+        num1dot = true;
         num1 += dot.textContent;
-    } else if (operator != "") num2 += dot.textContent;
+        display.append(dot.textContent);
+    } else if (operator != "" && num2dot == false) {
+        num2dot = true;
+        num2 += dot.textContent;
+        display.append(dot.textContent);
+    }
 });
 
 // to calculate the values
@@ -64,6 +79,7 @@ function plus(a, b) {
     num1 = result;
     num2 = "";
     display.textContent = result;
+    num2dot = false;
 }
 function minus(a, b) {
     result = a - b;
@@ -71,20 +87,25 @@ function minus(a, b) {
     num1 = result;
     num2 = "";
     display.textContent = result;
+    num2dot = false;
 }
 function multiply(a, b) {
     result = a * b;
+    result = Math.round(result * 1000) / 1000;
     operator = "";
     num1 = result;
     num2 = "";
     display.textContent = result;
+    num2dot = false;
 }
 function divide(a, b) {
     result = a / b;
+    result = Math.round(result * 1000) / 1000;
     operator = "";
     num1 = result;
     num2 = "";
     display.textContent = result;
+    num2dot = false;
 }
 
 // check which function corresponds, depending on the operator
